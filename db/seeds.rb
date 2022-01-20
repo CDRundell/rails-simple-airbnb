02fcc71141f944c5ff5bef4cff0e2e3250de6465
Chris Rundell <cdrundell@icloud.com>
2022-01-20 17:53:45 +0000

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'View of the Thames',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'Close to Kings X',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'The Ritz',
+  address: '150 Piccadilly London W1J 9BR',
+  description: '$$$',
+  price_per_night: 800,
+  number_of_guests: 2
+)
